97d38f7d4f5f02eff051942e4f274721a9ae35a5
wellcerv <wellcerv@hotmail.com>
2019-09-18 21:52:07 -0300

    Alteracao Pesquisa Usuario

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -3,34 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class HomeController extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
+	
+	public function index()	{
+
 		// Carrega somente a View BuscarUsuário
 		// $this->load->view('BuscarUsuario');
 
 		// Faz a leitura do Modelo Usuario_model	
 		$this->load->model('usuario_model');
 
-		// dados['usuarios'] ira receber a consulta feita no metodo BuscarNome que esta
+		// dados['usuarios'] ira receber a consulta feita no metodo listarTodos que esta
 		//localizado no model Usuario_model
-		$dados['usuarios'] = $this->usuario_model->BuscarNome();
+		$dados['usuarios'] = $this->usuario_model->listarTodos();
 
 		// Carrega todos os templates dentro da View
 		$this->template->show('home', $dados);
+	}
+
+	// Função que buscará o metodo listarNome da model Usuario_model
+	public function pesquisaUsuario(){
+
+		$this->load->model('usuario_model');
+
+		$dados['listagem'] = $this->usuario_model->listarNome($_POST);
+
+		$this->template->show('home', $dados);
+
 	}
 }
