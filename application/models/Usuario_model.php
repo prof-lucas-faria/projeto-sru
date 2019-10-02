4376bab18dcd754c0ff4af59de4c3c2845f3dc01
andreiaass <phmanu@hotmail.com>
2019-10-01 22:33:01 -0300

    Alteracao funcoes usuarios

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -2,20 +2,20 @@
 
 class Usuario_model extends CI_Model{    
     
-    // Metodo para listar todos os usuários da tabela Usuario do banco SRU
+    // Metodo para listar todos os usuários da tabela Usuario do banco SRU. Essa função é utilizada Homecontrole.
     public function listarTodos(){
         return $this->db->get('Usuario');
     }
 
-    // Metodo para listar os usuários por nome utilizado pela classe controler pesquisaUsuario
-    public function listarNome($lista){
+    // Metodo para listar os usuários por matricula utilizado pela classe controler HomeController
+    public function listarMatricula($lista){
 
         if(empty($lista))
             return array();
         
         $lista = $this->input->post('busca');
 
-        $this->db->like('nome', $lista);
+        $this->db->like('matricula', $lista);
 
         $query = $this->db->get('Usuario');
 
