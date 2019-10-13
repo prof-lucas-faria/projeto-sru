4af2b6f2c3c114fe13190f83f477244e3cc8d0ff
[AnaPaulaMatos] <[ateodorodematos22@hotmail.com]>
2019-10-13 12:40:07 -0300

    Inserindo a session no autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -60,9 +60,7 @@ $autoload['packages'] = array();
   |	$autoload['libraries'] = array('user_agent' => 'ua');
  */
 
-$autoload['libraries'] = array('database', 'template', 'form_validation', 'session');
-
-$autoload['libraries'] = array('database', 'template', 'form_validation', 'pagination');
+$autoload['libraries'] = array('database', 'template', 'form_validation', 'session', 'pagination');
 
 /*
   | -------------------------------------------------------------------
