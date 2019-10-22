471d178b0e65d2bbd1472c4e4aa42dbbdeac8df4
wellcerv <wellcerv@hotmail.com>
2019-10-22 14:58:45 -0300

    Entrada gravando no banco

--- application/models/Movimento_model.php
+++ application/models/Movimento_model.php
@@ -4,11 +4,20 @@ class Movimento_model extends CI_Model{
 
     public function setEntrada($id){
 
-        $this->db->where('idUsuario', $id);
-        $this->db->limit(1);
-        $query = $this->db->get('usuario');
-        return $query->row();
+        // $this->db->where('idUsuario', $id);
+        // $this->db->limit(1);
+        // $query = $this->db->get('Usuario');
+
+        $query = $this->db->query("select * from Usuario, Cardapio where Usuario.idUsuario = $id and Cardapio.data = date(now())");
         
+        $dados['data'] = date('y-m-d');
+        $dados['idUsuarios'] = $query->row()->idUsuario;
+        $dados['idCardapio'] = $query->row()->idCardapio;
+        $dados['idTipoMovimento'] = '1';
+        $this->db->insert('Movimento', $dados);
+        return $dados;    
+        
+        // return $query->row();
         
     }
 
